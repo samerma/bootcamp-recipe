fe777a6cd7c896384dd6328ca6fd00dce3643de0
Samer <samahajna@gmail.com>
2020-05-06 14:08:02 +0300

    fix render

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -1,20 +1,4 @@
 class Renderer {
-    constructor() { }
-    render2 = function (input, recipeTemplate, resultsContainer) {
-
-        $.get(`recipes/${input}`, function (data) {
-            $("#" + resultsContainer).empty()
-            if (data !== 'error' && data.length != 0) {
-                const source = $('#' + recipeTemplate).html()
-                const template = Handlebars.compile(source)
-                let newHTML = template({ recipes: data })
-                $("#" + resultsContainer).append(newHTML)
-            }
-            else {
-                $("#" + resultsContainer).append(`<p>No Results!</p>`)
-            }
-        })
-    }
     render = function (data) {
         $("#results").empty()
         if (data !== 'error' && data.length != 0) {
